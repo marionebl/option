0eb0dfd13b0902645cfad005bc4aa4a4f6b4cd81
Mario Nebl <hello@mario-nebl.de>
2019-04-03 12:59:57 +0200

    fix: expose types correctly

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testRegex: undefined,
-    testMatch: ["**/*.test.ts?(x)"],
-    collectCoverageFrom: ["src/**/*.ts"],
-    moduleNameMapper: {
-      "@marionebl/option": "<rootDir>/src/index.ts"
-    }
-  };
-  
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: undefined,
+  testMatch: ["**/*.test.ts?(x)"],
+  collectCoverageFrom: ["src/**/*.ts"],
+  moduleNameMapper: {
+    "@marionebl/option": "<rootDir>/src/index.ts"
+  }
+};
